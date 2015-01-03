80d0d1150098832276ab8afc28c89dea14fbdb8b
Shigure Shimotori <S_Shimotori@Shigures-Mac-mini.local>
2015-01-03 20:48:51 +0900

    Initial Commit

---
+++ MorganiteModel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MorganiteModel
+//
+//  Created by Shigure Shimotori on 1/3/15.
+//  Copyright (c) 2015 Shigure Shimotori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
